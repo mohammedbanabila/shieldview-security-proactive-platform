957a9239b0519936029a3d3c22f3ada9004f8df9
mbanabila <mbanabila@mba0.Home>
2025-11-21 23:03:13 +0300

    news

---
+++ shields/backend/viewsecrets/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ViewsecretsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'viewsecrets'
---
+++ shields/backend/zerotrust/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ZerotrustConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'zerotrust'
